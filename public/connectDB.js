d138d10eda90088962d78694621a71548cdbb265
NastyaBesch <kopylovvap8@gmail.com>
2024-04-11 21:29:43 +0300

    Update database connection configuration

--- public/connectDB.js
+++ public/connectDB.js
@@ -1,16 +1,20 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
+const dbUrl =
+  "mysql://qbuk825x4tzh2ajw:nw0b0vgajtg9rvc1@ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bfl2k9dv3ddtrv6k";
+
+  const con = mysql.createConnection(dbUrl);
+  
 // Create a connection configuration object to connect to the MySQL database
-const con = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  queueLimit: 0,
-});
+// const con = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   queueLimit: 0,
+// });
 
 con.connect((err) => {
   if (err) {
