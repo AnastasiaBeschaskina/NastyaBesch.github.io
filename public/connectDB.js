62d715f3de25fe51aed39161b825db40adc8fde0
NastyaBesch <kopylovvap8@gmail.com>
2024-04-11 22:59:27 +0300

    Added connection to JawsDB MariaDB database

--- public/connectDB.js
+++ public/connectDB.js
@@ -1,13 +1,9 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const jawsDbUrl = process.env.JAWSDB_MARIA_URL;
+
+const con = mysql.createConnection(jawsDbUrl);
 
 con.connect((err) => {
   if (err) {
