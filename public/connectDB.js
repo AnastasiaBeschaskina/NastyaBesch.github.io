8d6615557720911d928457f549352bacc2d5fdec
NastyaBesch <kopylovvap8@gmail.com>
2024-04-11 19:49:27 +0300

    Add connectDB file for database connection setup

---
+++ public/connectDB.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+// Create a connection configuration object to connect to the MySQL database
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  queueLimit: 0,
+});
+
+con.connect((err) => {
+  if (err) {
+    console.log("Error, Failed to connect to the database", err);
+    return;
+  }
+  console.log("Connection Successful");
+});
+
+module.exports = con;
