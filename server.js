f0aa217ae69e03ec50ebf09c38ddf475ebea68f0
NastyaBesch <kopylovvap8@gmail.com>
2024-04-15 11:15:45 +0300

    Remove host binding to allow Heroku to set it

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 const cors = require("cors");
 const cron = require("node-cron");
 const bcrypt = require("bcrypt");
